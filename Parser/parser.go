a0cdcb85d29cd90bffa7caff6411e63a8a8b4744
Kamit <timaplay137@gmail.com>
2025-03-15 20:55:40 +0300

    паарсер парсит, но с оговорками... большими

--- Parser/parser.go
+++ Parser/parser.go
@@ -1,44 +1,67 @@
 package main
 
 import (
+	"encoding/json"
+	"flag"
 	"fmt"
+	"io"
 	"log"
 	"net/http"
 	"os"
-	"strings"
-
-	"github.com/PuerkitoBio/goquery"
-	//"golang.org/x/net/html"
 )
 
-func Pars() {
-	for _, url := range os.Args[1:] {
-		links, err := findlinks(url)
-		if err != nil {
-			fmt.Println(os.Stderr, "Ошибка", err)
-		}
-		for _, link := range links {
-			fmt.Println(link)
-		}
+var nomenclaure_id string
+
+func initFlags() {
+	flag.StringVar(&nomenclaure_id, "nm_id", "256329171", "nomenclature_id example: 256329171")
+	flag.Parse()
+	if nomenclaure_id == "" {
+		fmt.Println("arg nm_id not found ")
+		os.Exit(0)
 	}
 }
 
-func findlinks(url string) ([]string, error) {
-	resp, err := http.Get(url)
-	if err != nil {
-		return nil, err
-	}
-	if resp.StatusCode != http.StatusOK {
-		resp.Body.Close()
-		return nil, fmt.Errorf("getting %s: %s", url, resp.Status)
-	}
-	doc, err := html.Parse(resp.Body)
-	resp.Body.Close()
-	if err != nil {
-		return nil, fmt.Errorf("parsing %s as html :%v ", url, err)
-	}
-	return visit(nil, doc), nil
+// JSON представляет структуру JSON, которую мы ожидаем получить.
+type JSON struct {
+	Data Data `json:"data"`
 }
 
-func visit(links []string, n *html.Node) []string {
+type Data struct {
+	Products []Product `json:"products"`
+}
+
+type Product struct {
+	Name       string `json:"name"`
+	SalePriceU int    `json:"salePriceU"`
+}
+
+func fail(err error, message string) {
+	if err != nil {
+		log.Fatalf("%s: %s", message, err)
+	}
+}
+
+func main() {
+	initFlags()
+
+	url := "https://card.wb.ru/cards/v2/detail?appType=1&curr=rub&dest=-1255987&spp=30&ab_testing=false&lang=ru&nm=" + nomenclaure_id
+	result, err := http.Get(url)
+	fail(err, "request EB fail")
+
+	if result.StatusCode > 299 {
+		fmt.Println("error ", result.StatusCode)
+		os.Exit(0)
+	}
+
+	body, err := io.ReadAll(result.Body)
+	fail(err, "read body")
+
+	fmt.Printf("%s\n", body)
+
+	var jsonData JSON
+
+	err = json.Unmarshal(body, &jsonData)
+	fail(err, "json parser")
+
+	fmt.Println(jsonData)
 }
